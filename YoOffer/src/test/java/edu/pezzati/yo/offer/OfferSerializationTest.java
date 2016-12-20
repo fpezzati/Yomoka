d2b6dcf91117ea30d9bda6a22ae0625874cbf2d1
Francesco Pezzati <fpezzati@gmail.com>
2016-12-20 22:26:41 +0100

    First test runs fine.

--- YoOffer/src/test/java/edu/pezzati/yo/offer/OfferSerializationTest.java
+++ YoOffer/src/test/java/edu/pezzati/yo/offer/OfferSerializationTest.java
@@ -1,32 +1,32 @@
 package edu.pezzati.yo.offer;
 
-import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import edu.pezzati.yo.offer.model.Offer;
-import edu.pezzati.yo.offer.model.OfferSerializer;
 
 public class OfferSerializationTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
-    public void serializeNullOffer() {
-	Logger log = Logger.getLogger(getClass());
+    public void serializeEmptyOffer() throws JsonProcessingException {
 	Offer offer = new Offer();
 	ObjectMapper objectMapper = new ObjectMapper();
-	SimpleModule offerModule = new SimpleModule();
-	offerModule.addSerializer(Offer.class, new OfferSerializer());
-	objectMapper.registerModule(offerModule);
-	String emptyOffer = "{}";
-	try {
-	    Assert.assertEquals(emptyOffer, objectMapper.writeValueAsString(offer));
-	} catch (JsonProcessingException e) {
-	    log.error(e);
-	}
+	expectedException.expect(JsonMappingException.class);
+	objectMapper.writeValueAsString(offer);
 	Assert.fail();
+    }
+
+    public void serializeFullyValuedOffer() {
+	Offer offer = new Offer();
+	ObjectMapper objectMapper = new ObjectMapper();
     }
 }
