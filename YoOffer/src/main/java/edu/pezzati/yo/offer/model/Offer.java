2890928751b4b2757ce495429497c948e6f3d103
Francesco Pezzati <fpezzati@gmail.com>
2016-12-26 23:18:10 +0100

    Ok. I got two tests working. Now I want to monitor test coverage so I add jacoco. I have to figure out how to aggregate module reports in parent project.

--- YoOffer/src/main/java/edu/pezzati/yo/offer/model/Offer.java
+++ YoOffer/src/main/java/edu/pezzati/yo/offer/model/Offer.java
@@ -1,5 +1,7 @@
 package edu.pezzati.yo.offer.model;
 
+import java.util.Objects;
+
 import org.bson.types.ObjectId;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -51,62 +53,21 @@ public class Offer {
 
     @Override
     public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((desc == null) ? 0 : desc.hashCode());
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	result = prime * result + ((lat == null) ? 0 : lat.hashCode());
-	result = prime * result + ((lon == null) ? 0 : lon.hashCode());
-	result = prime * result + ((ownerId == null) ? 0 : ownerId.hashCode());
-	result = prime * result + ((price == null) ? 0 : price.hashCode());
-	result = prime * result + ((title == null) ? 0 : title.hashCode());
-	return result;
+	return Objects.hash(id, title, desc, ownerId, price, lat, lon);
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
+	if (obj == null) {
 	    return false;
-	if (getClass() != obj.getClass())
+	}
+	if (getClass() != obj.getClass()) {
 	    return false;
-	Offer other = (Offer) obj;
-	if (desc == null) {
-	    if (other.desc != null)
-		return false;
-	} else if (!desc.equals(other.desc))
-	    return false;
-	if (id == null) {
-	    if (other.id != null)
-		return false;
-	} else if (!id.equals(other.id))
-	    return false;
-	if (lat == null) {
-	    if (other.lat != null)
-		return false;
-	} else if (!lat.equals(other.lat))
-	    return false;
-	if (lon == null) {
-	    if (other.lon != null)
-		return false;
-	} else if (!lon.equals(other.lon))
-	    return false;
-	if (ownerId == null) {
-	    if (other.ownerId != null)
-		return false;
-	} else if (!ownerId.equals(other.ownerId))
-	    return false;
-	if (price == null) {
-	    if (other.price != null)
-		return false;
-	} else if (!price.equals(other.price))
-	    return false;
-	if (title == null) {
-	    if (other.title != null)
-		return false;
-	} else if (!title.equals(other.title))
-	    return false;
-	return true;
+	}
+	final Offer offer = (Offer) obj;
+	return Objects.equals(this.id, offer.id) && Objects.equals(this.title, offer.title)
+		&& Objects.equals(this.desc, offer.desc) && Objects.equals(this.ownerId, offer.ownerId)
+		&& Objects.equals(this.price, offer.price) && Objects.equals(this.lat, offer.lat)
+		&& Objects.equals(this.lon, offer.lon);
     }
 }
--- YoOffer/src/test/java/edu/pezzati/yo/offer/OfferDeserializationTest.java
+++ YoOffer/src/test/java/edu/pezzati/yo/offer/OfferDeserializationTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.bson.types.ObjectId;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,33 +13,39 @@ import edu.pezzati.yo.offer.model.Offer;
 
 public class OfferDeserializationTest {
 
+    private ObjectMapper objectMapper;
+    private ObjectId offerId;
+    private ObjectId ownerId;
+    private String offer;
+
+    @Before
+    public void init() {
+	objectMapper = new ObjectMapper();
+	offerId = new ObjectId();
+	ownerId = new ObjectId();
+	offer = null;
+    }
+
     @Test
     public void deserializeEmptyOffer() throws IOException {
-	String offer = "{}";
-	ObjectMapper objectMapper = new ObjectMapper();
-	Offer expectedOffer = new Offer();
-	Offer actualOffer = objectMapper.readValue(offer, Offer.class);
-	Assert.assertEquals(expectedOffer, actualOffer);
+	offer = "{}";
+	Offer expected = new Offer();
+	Offer actual = objectMapper.readValue(offer, Offer.class);
+	Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void deserializeFullyValuedOffer() throws IOException {
-	ObjectId offerId = new ObjectId();
-	ObjectId ownerId = new ObjectId();
-	String offer = "{\"id\":\"" + offerId.toString()
-		+ "\",\"title\":\"some title\",\"desc\":\"some desc\",\"ownerId\":\"" + ownerId.toString()
-		+ "\",\"price\":10.0,\"lat\":10.0,\"lon\":10.0}";
-	ObjectMapper objectMapper = new ObjectMapper();
-	Offer expectedOffer = new Offer(offerId, "some title", "some desc", ownerId, 10D, 10D, 10D);
-	Offer actualOffer = objectMapper.readValue(offer, Offer.class);
-	Assert.assertEquals(expectedOffer, actualOffer);
+	offer = "{\"id\":\"" + offerId.toString() + "\",\"title\":\"some title\",\"desc\":\"some desc\",\"ownerId\":\""
+		+ ownerId.toString() + "\",\"price\":10.0,\"lat\":10.0,\"lon\":10.0}";
+	Offer expected = new Offer(offerId, "some title", "some desc", ownerId, 10D, 10D, 10D);
+	Offer actual = objectMapper.readValue(offer, Offer.class);
+	Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void deserializePartiallyValuedOffer() throws IOException {
-	ObjectId ownerId = new ObjectId();
-	String offer = "{\"title\":\"some title\",\"desc\":\"some desc\",\"ownerId\":\"" + ownerId.toString() + "\"}";
-	ObjectMapper objectMapper = new ObjectMapper();
+	offer = "{\"title\":\"some title\",\"desc\":\"some desc\",\"ownerId\":\"" + ownerId.toString() + "\"}";
 	Offer expected = new Offer(null, "some title", "some desc", ownerId, null, null, null);
 	Offer actual = objectMapper.readValue(offer, Offer.class);
 	Assert.assertEquals(expected, actual);
