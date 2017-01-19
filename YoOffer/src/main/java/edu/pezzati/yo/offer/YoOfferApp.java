91ebd53d73195109b5e96f3cc3e4aa0ae9a61a61
francesco <francesco.pezzati@iia.cnr.it>
2017-01-19 17:40:46 +0100

    After a first try on wildfly-swarn I decide to fall back to old school wildfly and using a docker-compose wildfly + mongodb conf to run IT with maven. Let's configure it all. WIP.

---
+++ YoOffer/src/main/java/edu/pezzati/yo/offer/YoOfferApp.java
@@ -0,0 +1,17 @@
+package edu.pezzati.yo.offer;
+
+import javax.ws.rs.core.Application;
+
+public class YoOfferApp extends Application {
+
+	// Logger log = LoggerFactory.getLogger(getClass());
+	//
+	// public static void main(String[] args) {
+	// YoOfferApp yoApp = new YoOfferApp();
+	// yoApp.init();
+	// }
+	//
+	// private void init() {
+	// log.info("init");
+	// }
+}
