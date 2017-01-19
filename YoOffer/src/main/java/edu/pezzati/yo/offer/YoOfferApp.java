c0132b3800408ac9be44af3be01f25db93cf9834
Francesco Pezzati <fpezzati@gmail.com>
2017-01-19 19:40:30 +0100

    Got a conf class to startup restful services.

---
+++ YoOffer/src/main/java/edu/pezzati/yo/offer/YoOfferApp.java
@@ -0,0 +1,9 @@
+package edu.pezzati.yo.offer;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/yo")
+public class YoOfferApp extends Application {
+
+}
